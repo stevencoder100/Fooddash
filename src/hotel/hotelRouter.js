58b136df40456829f85643f5882b8b008f5ef4c7
Rayar Singh <128575585+stevencoder100@users.noreply.github.com>
2025-03-04 08:46:03 +0530

    hotelRouter.js

--- src/hotel/hotelRouter.js
+++ src/hotel/hotelRouter.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { senddata, senditems, sendcategories} = require("./hotelController");
+const { senddata, senditems, sendcategories, createOrder } = require("./hotelController");
 
 const hotelRouter = express.Router();
 
 hotelRouter.get("/", senddata);
 hotelRouter.get("/items", senditems);
 hotelRouter.get("/categories", sendcategories);
+hotelRouter.post("/order", createOrder);
 
 module.exports = hotelRouter;
