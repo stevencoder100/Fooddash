7985a76fa88ac8df5b946deb31108533cf5c577c
Rayar Singh <128575585+stevencoder100@users.noreply.github.com>
2025-03-04 08:45:32 +0530

    hotelController.js

--- src/hotel/hotelController.js
+++ src/hotel/hotelController.js
@@ -1,31 +1,7 @@
-const { hotel, item, category } = require('./hotelModel');
+const { hotel, item, category, order } = require('./hotelModel');
 
 const senddata = async (req, res) => {
-    try 
-    {
-        const { pureveg, name} = req.query;
-        const queryobject = {};
-
-        if(pureveg)
-        {
-            queryobject.pureveg = pureveg;
-        }
-        if(name)
-        {
-            queryobject.name = name;
-        }
-        const data = await hotel.find(queryobject);
-        res.status(200).json(data); 
-    } 
-    catch (err) 
-    {
-        res.status(500).json({ message: "Internal Server Error", err });
-    }
-};
-
-const senditems = async (req, res) => {
-    try
-    {
+    try {
         const { pureveg, name } = req.query;
         const queryobject = {};
 
@@ -33,28 +9,67 @@ const senditems = async (req, res) => {
             queryobject.pureveg = pureveg;
         }
 
-        if(name)
-        {
+        if (name) {
+            queryobject.name = name;
+        }
+        const data = await hotel.find(queryobject);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({ message: "Internal Server Error", err });
+    }
+};
+
+const senditems = async (req, res) => {
+    try {
+        const { pureveg, name } = req.query;
+        const queryobject = {};
+
+        if (pureveg) {
+            queryobject.pureveg = pureveg;
+        }
+
+        if (name) {
             queryobject.name = name;
         }
         const data = await item.find(queryobject);
         res.status(200).json(data);
-    }
-    catch (err) {
+    } catch (err) {
         res.status(500).json({ message: "Internal Server Error", err });
     }
 };
 
 const sendcategories = async (req, res) => {
-    try
-    {
+    try {
         const data = await category.find(req.query);
         res.status(200).json(data);
-    }
-    catch(err)
-    {
+    } catch (err) {
         console.log(err);
     }
 };
 
-module.exports = { senddata, senditems, sendcategories };
+const createOrder = async (req, res) => {
+    try {
+        const { phone, email, username, restaurantName, foodItems, total } = req.body;
+
+        if (!phone || !email || !username || !restaurantName || !foodItems || !total) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const newOrder = new order({
+            phone,
+            email,
+            username,
+            restaurantName,
+            foodItems,
+            total
+        });
+
+        await newOrder.save();
+        return res.status(200).json({ message: "Order created successfully" });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "Failed to create order" });
+    }
+};
+
+module.exports = { senddata, senditems, sendcategories, createOrder };
